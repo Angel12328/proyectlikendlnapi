fa366887cab0f534b7d5107d58a2f5572bd075e5
Angel12328 <angelzavalapadilla@gmail.com>
2025-06-19 22:32:20 -0600

    tercer commit

---
+++ LikendlnApi/Data/DbContextProyect.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace LikendlnApi.Data
+{
+    public class DbContextProyect : DbContext
+    {
+      //public DbContextProyect(DbContextOptions<DbContextProyect> options) :base(options) { }
+
+    }
+}
